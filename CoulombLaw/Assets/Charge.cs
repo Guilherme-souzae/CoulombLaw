4f5369bfc9209f3116a12471f0e9d04715f89e56
Guilherme-souzae <guilhermesouzae.contato@gmail.com>
2025-11-28 23:33:53 -0300

    arrastamento

--- CoulombLaw/Assets/Charge.cs
+++ CoulombLaw/Assets/Charge.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Charge : MonoBehaviour
 {
-    [Tooltip("Carga elétrica simulada, já escalonada pelo ChargeManager.")]
     public float charge;
-
-    [Tooltip("Massa simulada, já escalonada pelo ChargeManager.")]
     public float simulatedMass;
 
     private Rigidbody2D rb;
     private SpriteRenderer sr;
-    private SphereCollider collider;
+
+    private bool isDragging = false;
+    private Vector2 dragOffset;
+
+    [Header("Arrasto com ForÃ§a")]
+    public float dragForce = 40f;
+    public float maxDragSpeed = 25f;
+
+    [Header("Atrito Durante Arrasto")]
+    public float dragDamping = 8f;  // Quanto maior, mais lento o movimento durante arrasto
 
     private void Awake()
     {
@@ -18,21 +25,61 @@ public class Charge : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
     }
 
-    // Chamado logo após a instância da prefab
     public void Initialize(float scaledCharge, float scaledMass)
     {
-        this.charge = scaledCharge;
-        this.simulatedMass = scaledMass;
+        charge = scaledCharge;
+        simulatedMass = scaledMass;
 
-        rb.mass = scaledMass;   // massa escalonada
-
-        // A cor continua igual
+        rb.mass = scaledMass;
         sr.color = scaledCharge > 0 ? Color.red : Color.blue;
+    }
+
+    private void Update()
+    {
+        Vector2 mouseWorld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        if (Mouse.current.middleButton.wasPressedThisFrame)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorld, Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject == this.gameObject)
+            {
+                isDragging = true;
+                dragOffset = rb.position - mouseWorld;
+            }
+        }
+
+        if (Mouse.current.middleButton.wasReleasedThisFrame)
+        {
+            isDragging = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDragging)
+        {
+            Vector2 mouseWorld = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector2 targetPos = mouseWorld + dragOffset;
+
+            Vector2 dir = targetPos - rb.position;
+
+            // ForÃ§a de arrasto tipo mola
+            rb.AddForce(dir * dragForce);
+
+            // ATRITO DURANTE O ARRASTO (damping suave)
+            rb.linearVelocity *= Mathf.Exp(-dragDamping * Time.fixedDeltaTime);
+
+            // Limitar velocidade mÃ¡xima
+            if (rb.linearVelocity.magnitude > maxDragSpeed)
+                rb.linearVelocity = rb.linearVelocity.normalized * maxDragSpeed;
+        }
     }
 
     private void OnBecameInvisible()
     {
-        ChargeManager.Instance.RemoveCharge(rb);
+        if (ChargeManager.Instance != null)
+            ChargeManager.Instance.RemoveCharge(rb);
+
         Destroy(gameObject);
     }
 }
